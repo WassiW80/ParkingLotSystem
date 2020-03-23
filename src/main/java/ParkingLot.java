a9e589c9f8d539a8f5c982a6bd4e6c046bc35d1c
WassiW80 <sayedwaseeyat111@gmail.com>
2020-03-23 11:29:22 +0530

    ADDED: Code and test for unparking a vehicle.

--- src/main/java/ParkingLot.java
+++ src/main/java/ParkingLot.java
@@ -1,6 +1,17 @@
 public class ParkingLot {
 
+    private Object vehicle;
+
     public boolean isParked(Object vehicle) {
+        this.vehicle = vehicle;
         return true;
+    }
+
+    public boolean isUnParked(Object vehicle) {
+        if (this.vehicle.equals(vehicle)) {
+            this.vehicle = null;
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,13 +1,28 @@
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class ParkingLotTest {
+    Object vehicle;
+    ParkingLot parkingLot;
+
+    @Before
+    public void setUp() {
+        vehicle = new Object();
+        parkingLot = new ParkingLot();
+    }
+
     @Test
     public void givenAVehicleToPark_WhenParked_ShouldReturnTrue() {
-        Object vehicle = new Object();
-        ParkingLot parkingLot = new ParkingLot();
         boolean isParked = parkingLot.isParked(vehicle);
         assertTrue(isParked);
+    }
+
+    @Test
+    public void givenAVehicleToUnParked_WhenUnParked_ShouldReturnTrue() {
+        parkingLot.isParked(vehicle);
+        boolean isUnParked = parkingLot.isUnParked(vehicle);
+        assertTrue(isUnParked);
     }
 }
